1763915d30a0684e60a4b5f5750e53ab5e7a1d21
Xarius <xarius@xarius.io>
2021-08-13 08:41:54 -0400

    Added GPL3.

--- players.sh
+++ players.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-# players.sh
-# xarius@xarius.io
+# https://github.com/Xarius86/players.sh
+
 # Script to generate a list of all players that have joined a server.
 # Place in /server/world/playerdata folder. Make executable, and run.
 # Will generate players.txt that includes player names and UUID's.
-# REQUIRES curl and jq to be installed on your system.
-# REQUIRES nbted from https://github.com/C4K3/nbted
+# REQUIRES 'curl' and 'jq' to be installed on your system.
+# REQUIRES 'nbted' from https://github.com/C4K3/nbted
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # Remove any existing players.txt file.
 OLD_FILE="players.txt";
